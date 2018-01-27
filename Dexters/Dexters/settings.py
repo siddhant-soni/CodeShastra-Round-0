6582f66177d3d5972882d216439265b8dc1e8d59
Siddhant Soni <primeoptimus98@gmail.com>
2018-01-27 15:19:18 +0530

    Changed structure

--- Dexters/Dexters/settings.py
+++ Dexters/Dexters/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'Dexters.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoproject',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
---
+++ Dexters/myapp/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.db.models.fields import DateTimeField
+# Create your models hereself
+
+class User(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    email_ID=models.EmailField(max_length=30, primary_key=True)
+    user_name=models.CharField(max_length=30)
+    password=models.CharField(max_length=30)
+
+
+class Question(models.Model):
+    date=models.DateTimeField()
+    question=models.TextField(primary_key=True)
+
+
+
+class Answer(models.Model):
+    date=DateTimeField()
+    answer=models.TextField(primary_key=True)
+    no_of_upvotes=models.BigIntegerField()
+    no_of_downvotes=models.BigIntegerField()
+
+
+class Article(models.Model):
+    date=models.DateTimeField()
+    title=models.CharField(max_length=100, primary_key=True)
+    content=models.TextField()
+    no_of_upvotes=models.BigIntegerField()
+    no_of_downvotes=models.BigIntegerField()
+    no_of_views=models.BigIntegerField()
+
+
+class Comment(models.Model):
+    date=models.DateTimeField()
+    content=models.TextField(primary_key=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    no_of_upvotes=models.BigIntegerField()
+    no_of_downvotes=models.BigIntegerField()
