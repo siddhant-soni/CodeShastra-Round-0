3450030f0aceabe4199a66b1a550472afdd79ad8
Vishesh Vohra <visheshvohra@gmail.com>
2018-01-28 09:39:49 +0530

    Modifed models.py

--- Dexters/myapp/models.py
+++ Dexters/myapp/models.py
@@ -7,9 +7,10 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name=models.CharField(max_length=30)
-    last_name=models.CharField(max_length=30)
-    email_ID=models.EmailField(max_length=30, primary_key=True)
+    #first_name=models.CharField(max_length=30)
+    #last_name=models.CharField(max_length=30)
+    #email_ID=models.EmailField(max_length=30, primary_key=True)
+    #question=models.ForeignKey(Question, null=True)
     # user_name=models.CharField(max_length=30)
     # password=models.CharField(max_length=30)
 @receiver(post_save, sender=User)
@@ -22,33 +23,61 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
-
 class Question(models.Model):
     date=models.DateTimeField(auto_now=True)
     question=models.TextField(primary_key=True)
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    tags=models.ManyToManyField(tags)
+
+    def __str__(self):
+        return self.question
 
 class Answer(models.Model):
     date=DateTimeField(auto_now=True)
     answer=models.TextField(primary_key=True)
+    question=models.OneToOneField(Question, on_delete=CASCADE)
     no_of_upvotes=models.BigIntegerField(null=True)
     no_of_downvotes=models.BigIntegerField(null=True)
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    tags=models.ManyToManyField(tags)
+
+    def __str__(self):
+        return self.answer
 
 
 class Article(models.Model):
     date=models.DateTimeField(auto_now=True)
-    title=models.CharField(helptext="helptext",max_length=100, primary_key=True)
+    title=models.CharField(max_length=100, primary_key=True)
     content=models.TextField()
     no_of_upvotes=models.BigIntegerField(null=True)
     no_of_downvotes=models.BigIntegerField(null=True)
     no_of_views=models.BigIntegerField(null=True)
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    tags=models.ManyToManyField(tags)
+    
+    def __str__(self):
+        return self.title
+
 
 
 class Comment(models.Model):
     date=models.DateTimeField(auto_now=True)
     content=models.TextField(primary_key=True)
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    profile=models.OneToOneField(Profile, on_delete=models.CASCADE)
     no_of_upvotes=models.BigIntegerField(null=True)
     no_of_downvotes=models.BigIntegerField(null=True)
+    answer=models.ForeignKey(Answer, on_delete=CASCADE)
+    article=models.ForeignKey(Article, on_delete=CASCADE)
+    tags=models.ManyToManyField(tags)
+
+    def __str__(self):
+        return self.content
+
+class tags(models.Model):
+    topic=models.CharField(max_length=50)
+    auto_increment_id=models.AutoField(primary_key=True)
+    profile=models.ForeignKey(Profile, null=True)
+
+    def __str__(self):
+        return self.topic
+
